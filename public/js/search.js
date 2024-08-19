58ca8be0629b09712e1f05bad9aef0eadecee14f
henesbzdmr <concinik42@gmail.com>
2024-08-19 13:43:55 +0300

    ilk github deneyimim

---
+++ public/js/search.js
@@ -0,0 +1,8 @@
+const searchIcon = document.getElementById('searchIcon')
+const searchEngine = document.getElementById('searchEngine')
+
+
+
+searchIcon.addEventListener('click',()=> {
+    searchEngine.classList.toggle('active')
+})
